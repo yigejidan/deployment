410cd971eb5bc108aff1ab4e3996856c1b0dd59d
unknown <1051532683@qq.com>
2019-05-15 18:29:56 +0800

    enter

--- deploy/restart/restart.go
+++ deploy/restart/restart.go
@@ -1,12 +1,32 @@
 package restart
 
 import (
-
+	"deployment/host/monitor"
+	"deployment/host/connent"
+	"log"
+	"strings"
 )
 
 //ReStartComponent ：重启意外关闭的组件 
-func ReStartComponent () error {
-
+func ReStartComponent (host connent.Host,componentname string) error {
+	isStart,err := monitor.AuditComponent(host,componentname)
+	if err != nil {
+		log.Fatal("auditcomponent err",err)
+	}
+	if isStart == true {
+		log.Fatal(componentname,"Component is running")
+	}
+	result,err := connent.RunCommand(host, "docker ps -a")
+	isCon := strings.Contains(result,componentname)
+	if isCon == true {
+		cmd := "docker container start " + componentname
+		_,err := connent.RunCommand(host, cmd)
+		if err != nil {
+			log.Fatal("restart component err",err)
+		}
+	}else {
+		log.Fatal("docker no container for this component")
+	}
 	return nil 
 }
 
