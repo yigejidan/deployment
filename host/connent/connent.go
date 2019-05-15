5cf527128b3880e741456344290aed70a2f58bea
unknown <1051532683@qq.com>
2019-05-15 16:02:48 +0800

    enter

--- host/connent/connent.go
+++ host/connent/connent.go
@@ -1,23 +1,24 @@
-package host
+package connent
 
 import (
-    "fmt"
+    // "fmt"
     "bytes"
     "golang.org/x/crypto/ssh"
-    "deployment/getconf"
+    // "deployment/getconf"
     "log"
 	"net"
 	"sync"
 )
 
 var m *sync.Mutex
-config,err := getconf.ReadConfig(path)   //也可以通过os.arg或flag从命令行指定配置文件路径
-if err != nil {
-	log.Fatal(err)
-	return err
-}
 
-//ConnentHost 连接主机主程序
+// config,err := getconf.ReadConfig(path)   //也可以通过os.arg或flag从命令行指定配置文件路径
+// if err != nil {
+// 	log.Fatal(err)
+// 	return err
+// }
+
+//ConnentHost 连接主机主程序,发送命令并执行
 func ConnentHost(user string,password string,port string,command string) (result string,err error) {
 	m.Lock()
 	defer m.Unlock()
@@ -33,20 +34,20 @@ func ConnentHost(user string,password string,port string,command string) (result
 	}
 	client, err := ssh.Dial("tcp",port,clientconfig)
 	if err != nil {
-		log.Fatal(err)
-		return err
+		log.Fatal("connent post err",err)
+		return "",err
 	}
 	session, err := client.NewSession()
 	if err != nil {
 		log.Fatal(err)
-		return err
+		return "",err
 	}
 	defer session.Close()
 	var b bytes.Buffer
 	session.Stdout = &b
 	if err := session.Run(command); err != nil {
-		log.Fatal(err)
-		return err
+		log.Fatal("run command err",err)
+		return "",err
 	}
-	return b.string(),nil
+	return b.String(),nil
 }
--- host/monitor/getstatus.go
+++ host/monitor/getstatus.go
@@ -2,9 +2,25 @@ package monitor
 
 import (
 	"deployment/host/connent"
-	
+	"strings"
+	"log"
 )
 
-func MonitorComponents(user string,password string,port string,componentname string) (status string,err error) {
-	
+//AuditComponent 监视组件运行状态
+func AuditComponent(user string,password string,port string,componentname string) (status bool,err error) {
+	var ComponentState bool //组件正常运行为true，关闭为false
+	command := "docker ps"
+	result,err := connent.ConnentHost(user,password,port,command)
+	if err != nil {
+		log.Fatal("docker ps run err",err)
+	}
+	isCon := strings.Contains(result,componentname)
+	if isCon == true {
+		ComponentState = true
+		return ComponentState,nil
+	}else {
+		ComponentState = false 
+		return ComponentState,nil
+	}
 }
+
