1a418f59e47a2435120b71785698016a9b4b249f
unknown <1051532683@qq.com>
2019-05-16 15:29:20 +0800

    enter

--- host/monitor/getstatus.go
+++ host/monitor/getstatus.go
@@ -28,3 +28,21 @@ func AuditComponent(host *connent.Host,componentname string) (status bool,err er
 	}
 }
 
+//IsHaveContainer : 查看docker里是否有该组件的container
+func IsHaveContainer(host *connent.Host,componentname string) (bool,error) {
+	var IsHave bool //组件正常运行为true，关闭为false
+	command := "docker ps -a"
+	result,err := connent.RunCommand(host,command)
+	if err != nil {
+		log.Fatal("docker ps -a run err",err)
+	}
+	isCon := strings.Contains(result,componentname)
+	if isCon == true {
+		IsHave = true
+		return IsHave,nil
+	}else {
+		IsHave = false 
+		return IsHave,nil
+	}
+}
+
