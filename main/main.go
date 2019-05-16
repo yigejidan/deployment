35f6e9aa8fd746c97bc8599c17a0a50b111884ac
unknown <1051532683@qq.com>
2019-05-16 14:16:23 +0800

    enter

--- main/main.go
+++ main/main.go
@@ -9,18 +9,18 @@ import(
 
 
 func main() {
-	pdhost,pdcomponent := getconf.ReadConfigToHost(path.PDpath)
-	err := component.OpenDocker(pdhost)
-	if err != nil {
-		log.Fatal("pdhost Open Docker err",err)
-	}
-	err = component.DeployComponent(pdcomponent)
-	if err != nil {
-		log.Fatal("start pd component failed",err)
-	}
+	// pdhost,pdcomponent := getconf.ReadConfigToHost(path.PDpath)
+	// err := component.OpenDocker(pdhost)
+	// if err != nil {
+	// 	log.Fatal("pdhost Open Docker err",err)
+	// }
+	// err = component.DeployComponent(pdcomponent)
+	// if err != nil {
+	// 	log.Fatal("start pd component failed",err)
+	// }
 	
 	tidb1host,tidb1component := getconf.ReadConfigToHost(path.TIDB1path)
-	err = component.OpenDocker(tidb1host)
+	err := component.OpenDocker(tidb1host)
 	if err != nil {
 		log.Fatal("tidb1host Open Docker err",err)
 	}
@@ -29,25 +29,25 @@ func main() {
 		log.Fatal("start tidb1 component failed",err)
 	}
 
-	tidb2host,tidb2component := getconf.ReadConfigToHost(path.TIDB2path)
-	err = component.OpenDocker(tidb2host)
-	if err != nil {
-		log.Fatal("tidb2host Open Docker err",err)
-	}
-	err = component.DeployComponent(tidb2component)
-	if err != nil {
-		log.Fatal("start tidb2 component failed",err)
-	}
+	// tidb2host,tidb2component := getconf.ReadConfigToHost(path.TIDB2path)
+	// err = component.OpenDocker(tidb2host)
+	// if err != nil {
+	// 	log.Fatal("tidb2host Open Docker err",err)
+	// }
+	// err = component.DeployComponent(tidb2component)
+	// if err != nil {
+	// 	log.Fatal("start tidb2 component failed",err)
+	// }
 
-	tikvhost,tikvcomponent := getconf.ReadConfigToHost(path.TIKVpath)
-	err = component.OpenDocker(tikvhost)
-	if err != nil {
-		log.Fatal("tikvhost Open Docker err",err)
-	}
-	err = component.DeployComponent(tikvcomponent)
-	if err != nil {
-		log.Fatal("start tikv component failed",err)
-	}
+	// tikvhost,tikvcomponent := getconf.ReadConfigToHost(path.TIKVpath)
+	// err = component.OpenDocker(tikvhost)
+	// if err != nil {
+	// 	log.Fatal("tikvhost Open Docker err",err)
+	// }
+	// err = component.DeployComponent(tikvcomponent)
+	// if err != nil {
+	// 	log.Fatal("start tikv component failed",err)
+	// }
 
 }
 
