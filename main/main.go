be531e348ce3bde54ea1c4f7712ab688a2e71653
unknown <1051532683@qq.com>
2019-05-16 08:58:37 +0800

    enter

--- main/main.go
+++ main/main.go
@@ -10,31 +10,6 @@ import(
 
 
 func main() {
-	// err := host.ConnentHost(path.PDpath)
-	// if err != nil {
-	// 	log.Fatal("deploy PD err",err)
-	// 	return
-	// }
-	// err = host.ConnentHost(path.TIDB1path)
-	// if err != nil {
-	// 	log.Fatal("deploy TIDB1 err",err)
-	// 	return
-	// }
-	// err = host.ConnentHost(path.TIDB2path)
-	// if err != nil {
-	// 	log.Fatal("deploy TIDB2 err",err)
-	// 	return
-	// }
-	// err = host.ConnentHost(path.TIKVpath)
-	// if err != nil {
-	// 	log.Fatal("deploy TIKV err",err)
-	// 	return
-	// }
-	// err = modify.Modify(path.Changepath,path.TIDB3path)
-	// if err != nil {
-	// 	log.Fatal("modify component err",err)
-	// 	return
-	// }
 	pdhost,pdcomponent := ReadConfigToHost(path.PDpath)
 	err := component.OpenDocker(pdhost)
 	if err != nil {
